847fbb10e988a31ebc7d11c041adb672c9cc2727
Naor Balas <naor56564@walla.com>
2026-05-16 17:23:28 +0300

    Class Stack

---
+++ q1.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <algorithm>
+class Stack{
+    private:
+        size_t m_cap;
+        int *m_stack;
+        size_t m_len;
+        int flag_enlarge=0,flag_reduce=0;
+        void resize(int &flag_enl,int &flag_redu){
+            if((flag_enl==0)&&(flag_redu==0)){
+                return;
+            }
+            else if((flag_enl==1)&&(flag_redu==1)){
+                std::cout<<"Error, flag enlrage and flage reduce can not be ON the same time\n";
+                std::cout<<"Both flags are now off\n";
+                flag_enl=0;
+                flag_redu=0;
+            }
+            else if((flag_enl==1)&&(flag_redu==0)){            
+                this->m_cap*=2;               
+                int *tmp=new int[m_cap];
+                if(tmp==nullptr){
+                    std::cout<<"Error, falied to allocated memory\n";                    
+                    exit(EXIT_FAILURE);
+                }
+                for(size_t i=0;i<m_len;i++){
+                    tmp[i]=m_stack[i];
+                }
+                delete [] m_stack;
+                m_stack=tmp;
+                flag_enl=0;
+            }
+            else if((flag_enl==0)&&(flag_redu==1)){
+                int *tmp=new int[m_len];
+                if(tmp==nullptr){
+                    std::cout<<"Error, falied to allocated memory\n";                    
+                    exit(EXIT_FAILURE);
+                }
+                for(size_t i=0;i<m_len;i++){
+                    tmp[i]=m_stack[i];
+                }
+                delete [] m_stack;
+                m_stack=tmp;
+                m_cap--;
+                flag_reduce=0;
+            }
+        }
+    public:
+        Stack(size_t cap=1):m_cap{cap},m_stack{new int[m_cap]},m_len{0}{
+            std::cout<<"C-tor was created for "<<this<<" object\n";
+        }
+        ~Stack(){
+            delete [] m_stack;
+            std::cout<<"D-Tor is called for "<<this<<'\n';
+        }
+        Stack(Stack &other):m_cap{other.m_cap},m_stack{new int[m_cap]},m_len{other.m_len}{            
+            for(size_t i=0;i<m_len;i++){
+                m_stack[i]=other.m_stack[i];
+            }
+            std::cout<<"Copy C-tor from "<<&other<<" to "<<this<<" was created\n";            
+        }
+        Stack & operator =(Stack &o){
+            if(this==&o){
+                return *this;
+            }
+            this->m_cap=o.m_cap;
+            this->m_len=o.m_len;
+            delete [] this->m_stack;
+            this->m_stack=new int[m_cap];
+            for(size_t i=0;i<m_len;i++){
+                m_stack[i]=o.m_stack[i];
+            }
+        }
+
+        Stack(Stack && o):m_cap{o.m_cap},m_stack{o.m_stack},m_len{o.m_len}{
+            std::cout<<"Move copy C-tor was called from "<<&o<<" to "<<this<<'\n';
+            o.m_stack=nullptr;
+
+        }
+        Stack & operator=(Stack &&o){
+            if (this==&o){
+                return *this;
+            }
+            delete [] m_stack;
+            m_cap=o.m_cap;
+            m_stack=o.m_stack;
+            m_len=o.m_len;
+            o.m_stack=nullptr;
+        }
+        // void resize(int &flag_enl,int &flag_redu){
+        //     if((flag_enl==0)&&(flag_redu==0)){
+        //         return;
+        //     }
+        //     else if((flag_enl==1)&&(flag_redu==1)){
+        //         std::cout<<"Error, flag enlrage and flage reduce can not be ON the same time\n";
+        //         std::cout<<"Both flags are now off\n";
+        //         flag_enl=0;
+        //         flag_redu=0;
+        //     }
+        //     else if((flag_enl==1)&&(flag_redu==0)){            
+        //         this->m_cap*=2;               
+        //         int *tmp=new int[m_cap];
+        //         if(tmp==nullptr){
+        //             std::cout<<"Error, falied to allocated memory\n";                    
+        //             exit(EXIT_FAILURE);
+        //         }
+        //         for(size_t i=0;i<m_len;i++){
+        //             tmp[i]=m_stack[i];
+        //         }
+        //         delete [] m_stack;
+        //         m_stack=tmp;
+        //         flag_enl=0;
+        //     }
+        //     else if((flag_enl==0)&&(flag_redu==1)){
+        //         int *tmp=new int[m_len];
+        //         if(tmp==nullptr){
+        //             std::cout<<"Error, falied to allocated memory\n";                    
+        //             exit(EXIT_FAILURE);
+        //         }
+        //         for(size_t i=0;i<m_len;i++){
+        //             tmp[i]=m_stack[i];
+        //         }
+        //         delete [] m_stack;
+        //         m_stack=tmp;
+        //         m_cap--;
+        //         flag_reduce=0;
+        //     }
+        // }
+        friend Stack operator + (Stack &o1,Stack &o2);
+        Stack & operator += (Stack const &o){
+            m_cap+=o.m_cap;
+            size_t idx_to_start=m_len;
+            flag_enlarge=1;
+            resize(flag_enlarge,flag_reduce);
+            m_len+=o.m_len;
+            for(size_t i=0;i<o.m_len;i++){
+                m_stack[idx_to_start]=o.m_stack[i];
+                idx_to_start++;
+            }
+            return *this;
+        }
+        Stack &push(int v){            
+            if(m_cap==1){                
+                m_cap*=2;
+            }
+            else if(m_len>=m_cap){
+                flag_enlarge=1;
+                resize(flag_enlarge,flag_reduce);
+                // m_cap*=2;
+                // int *tmp=new int[m_cap];
+                // if(tmp==nullptr){
+                //     std::cout<<"Error, falied to allocated memory\n";                    
+                //     exit(EXIT_FAILURE);
+                // }
+                // for(size_t i=0;i<m_len;i++){
+                //     tmp[i]=m_stack[i];
+                // }
+                // delete [] m_stack;
+                // m_stack=tmp;
+            }
+            m_stack[m_len]=v;
+            m_len++;
+            return *this;
+        }
+        Stack& pop(int &re){
+            if(m_len==0){
+                std::cout<<"Array is in size of zero, pop can't take place\n";
+                m_cap=0;
+                return *this;
+            }
+            re=m_stack[--m_len];
+            flag_reduce=1;
+            resize(flag_enlarge,flag_reduce);
+            // int *tmp=new int[m_len];
+            // if(tmp==nullptr){
+            //     std::cout<<"Error, falied to allocated memory\n";                    
+            //     exit(EXIT_FAILURE);
+            // }
+            // for(size_t i=0;i<m_len;i++){
+            //     tmp[i]=m_stack[i];
+            // }
+            // delete [] m_stack;
+            // m_stack=tmp;
+            // m_cap--;
+            return *this;
+        }
+        void print() const {
+            std::cout<<"print id: "<<this<<'\n';
+            if(m_len==0){
+                std::cout<<"Array is empty";
+            }
+            for(size_t i=0;i<m_len;i++){
+                std::cout<<m_stack[i]<<' ';
+            }
+            std::cout<<'\n';
+        }
+
+
+};
+Stack operator + (Stack &o1,Stack &o2){
+    Stack result{o1.m_cap+o2.m_cap};
+    size_t i=0;
+    for(;i<o1.m_len;i++){
+        result.m_stack[i]=o1.m_stack[i];
+
+    }
+    for(size_t j=0;j<o2.m_len;j++){
+        result.m_stack[i]=o2.m_stack[j];
+        i++;
+    }
+    result.m_len=i;
+    return result;
+}
+
+int main() {
+    //code
+    //for cout only: 
+    std::cout << std::unitbuf;
+    Stack s1{};
+    s1.push(12).push(56).push(15);
+    Stack s2{(s1)};
+    Stack s3=s1+s2;
+    std::cout<<"s1: \n";
+    s1.print();
+    std::cout<<"s3: \n";
+    s3.print();
+    std::cout<<"s3+s1: \n";
+    s3+=s1;
+    s3.print();
+    
+    return 0;
+}
